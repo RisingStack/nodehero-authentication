4e19b54a9bce439ee5551d3eff089e2e079019b7
Vasyl Dilai <babaikawow@gmail.com>
2018-08-17 10:42:15 +0300

    refactor

--- config/index.js
+++ config/index.js
@@ -1,9 +1,7 @@
 const config = {}
 
 config.redisStore = {
-  host: process.env.REDIS_STORE_HOST,
-  port: process.env.REDIS_STORE_PORT,
-  password: process.env.REDIS_STORE_PASSWORD
+  host: process.env.REDIS_STORE_HOST
 }
 
 module.exports = config
