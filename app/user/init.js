2545d456839f264005d270d43b70f221c0809ac9
Vasyl Dilai <babaikawow@gmail.com>
2018-08-17 10:31:22 +0300

    added new routes, changed redirect rules, added new view

--- app/user/init.js
+++ app/user/init.js
@@ -1,16 +1,21 @@
 const passport = require('passport')
 
 function initUser (app) {
-  app.get('/', renderWelcome)
+  app.get('/', passport.authenticationMiddleware(), renderWelcome)
+  app.get('/login', renderLogin)
   app.get('/profile', passport.authenticationMiddleware(), renderProfile)
   app.post('/login', passport.authenticate('local', {
-    successRedirect: '/profile',
-    failureRedirect: '/'
+    successRedirect: '/',
+    failureRedirect: '/login'
   }))
 }
 
 function renderWelcome (req, res) {
   res.render('user/welcome')
+}
+
+function renderLogin (req, res) {
+  res.render('user/login')
 }
 
 function renderProfile (req, res) {
